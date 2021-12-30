3dfc56ecff5e67f59238031be43d94be476c9a56
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2021-12-30 01:29:05 -0800

    Add files via upload

---
+++ weeks.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void main()
+{
+	int m;
+	printf("enter the number");
+	scanf("%d",&m);
+	if(m==1)
+	{
+		printf("monday");
+	}
+	else if(m==2)
+{
+	printf("tuesday");
+}
+else if(m==3)
+{
+	printf("wednesday");
+}
+else if(m==4)
+{
+	printf("thu");
+}
+else if(m==5)
+{
+	printf("fri");
+}
+else if(m==6)
+{
+	printf("sat");
+}
+else if(m==7)
+{
+	printf("sun");
+}
+else 
+{
+	printf("invalid input");
+}
+}
