44bbf35fd38b69c66ad3522137fac5cf64cfa39d
Jahnavi934 <96327245+Jahnavi934@users.noreply.github.com>
2021-12-30 01:06:08 -0800

    Add files via upload

---
+++ cost.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	int x,y;
+	printf("enter two numbers");
+	scanf("%d%d",&x,&y);
+	if(x<y)
+	{
+		printf("profit");
+}
+	else if(x==y)
+	
+	{printf("no profit no loss");
+}
+	else
+	printf("loss");
+}
